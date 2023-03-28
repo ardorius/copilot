0dcda712db427098491247087140aa98f9b9ed9a
Peter Petruš <petrus.peterhe@gmail.com>
2023-03-28 22:56:58 +0200

    empty - for demo purposes

--- 00-examples/build/error-fix.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function getArr() {
-    return [1, 2, 3];
-}
-function getObj3() {
-    return { name: 100, age: 30, country: 'Chile', city: 'Santiago' };
-}
--- 00-examples/src/object.ts
+++ 00-examples/src/object.ts
@@ -1,4 +1,3 @@
-// Object Literal Syntax (Object Literal) - Object Literal Syntax (Object Literal)
 interface Person {
     name: string;
     age: number;
@@ -11,33 +10,4 @@ interface Person {
     hobbies: string[];
     isMarried: boolean;
     children: Person[];
-}
-
-//create object person
-
-const person: Person = {
-    name: 'Peter',
-    age: 30,
-    address: {
-        street: 'Hlavna',
-        city: 'Bratislava',
-        country: 'Slovakia',
-    },
-    datebirth: new Date(1989, 1, 1),
-    hobbies: ['sport', 'music'],
-    isMarried: false,
-    children: [],
-    country: "",
-    city: ""
-};
-console.log(
-!person.country ? person.country = "Slovakia" : person.country
-);  //if person.country is empty, then person.country = "Slovakia"
-console.log(person);
-
-if (person.city === "") {
-console.log(person.city = "Bratislavax");
-}
-else {
-console.log(person.city);
 }
